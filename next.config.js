1edd5dbdc3f264348c933c6caa0eedacafc979a6
Felipe Dessotti Inoue <felipeinoue@hotmail.com>
2024-04-11 12:51:43 -0300

    standalone

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
-  // output: "standalone",
-  output: "node .next/standalone/server.js",
+  output: "standalone",
+  // output: "node .next/standalone/server.js",
 }
