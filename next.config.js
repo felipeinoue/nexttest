84785efd821fa9aa5fecd9c027f33e0549ad99a3
Felipe Dessotti Inoue <felipeinoue@hotmail.com>
2024-04-11 12:58:43 -0300

    allowed origins

--- next.config.js
+++ next.config.js
@@ -3,3 +3,21 @@ module.exports = {
   output: "standalone",
   // output: "node .next/standalone/server.js",
 }
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "website.com", 
+        "localhost:3000",
+        "http://15.229.77.132/next",
+        "http://15.229.77.132",
+        "*",
+      ]
+    }
+  }
+}
+
+module.exports = nextConfig
